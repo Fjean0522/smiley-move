af0e81a830fa38e79d3df4acd70b8761836c8aa9
Fjean <fjean0522@gmail.com>
2023-04-14 19:31:27 -0400

    Add comments explaining what code in .js file does

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const smiley = document.querySelector('.face')
 count = 0
 
 function moveSmiley() {
-    count += 50
-    smiley.style.left = count + 'px'
+    count += 50    
+    // Adds 50 to count when the function is invoked.
+    smiley.style.left = count + 'px' 
+     //Moves smiley to horizontaly by count number in px.
     console.log(`Smiley moved! ${count}`)
+    //Keeps track of smiley's moves.
 }
 
 smiley.addEventListener('click', moveSmiley)
+// Invokes "moveSmiley" function when smiley's face is clicked.
